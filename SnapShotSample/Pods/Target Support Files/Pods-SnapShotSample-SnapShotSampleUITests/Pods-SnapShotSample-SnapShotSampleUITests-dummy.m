7530bd19e8ee4339a77427cade03467f7bc159e7
Alexandre Vassinievski <alexvassini@gmail.com>
2019-10-05 12:02:53 -0300

    Pod init

---
+++ SnapShotSample/Pods/Target Support Files/Pods-SnapShotSample-SnapShotSampleUITests/Pods-SnapShotSample-SnapShotSampleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapShotSample_SnapShotSampleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapShotSample_SnapShotSampleUITests
+@end
---
+++ SnapShotSample/Pods/Target Support Files/Pods-SnapShotSampleTests/Pods-SnapShotSampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SnapShotSampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_SnapShotSampleTests
+@end
