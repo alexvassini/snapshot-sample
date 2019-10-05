a3d66870e08f54335abd0acb9609b074beb3cd7e
Alexandre Vassinievski <alexvassini@gmail.com>
2019-10-05 13:44:07 -0300

    fix something

--- SnapShotSample/Pods/Target Support Files/Pods-SnapShotSample-SnapShotSampleUITests/Pods-SnapShotSample-SnapShotSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SnapShotSample_SnapShotSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SnapShotSample_SnapShotSampleUITests
-@end
